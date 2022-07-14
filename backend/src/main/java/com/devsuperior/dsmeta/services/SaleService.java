67026dcd181f496be7bb745cbb31c4440f426a82
Caroline Ferraz <projetos.crrz@gmail.com>
2022-07-14 00:24:30 -0300

    feat: adicioando seleção por data

--- backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -1,8 +1,13 @@
 package com.devsuperior.dsmeta.services;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.devsuperior.dsmeta.entities.Sale;
@@ -14,8 +19,23 @@ public class SaleService {
 	@Autowired
 	private SaleRepository repository;
 	
-	public List<Sale> findSales() {
-		return repository.findAll();
+	public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable) {
+		
+		//Para capturar a data atual dentro da variável today:
+		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		
+		/* Expressão condicional ternária. Se minDate.equals("") for verdade, ou seja, seja nulo/vazio, max receberá 
+		 * today.minusDays(365), que é a data atual menos 365 dias. 
+		 * Se minDate.equals("") não seja vazio, ele receberá o LocalDate.parse(minDate) */
+		//O LocalDate.parse(minDate) serve para converter o tipo de dado String, do minDate, para o formato LocalDate
+		LocalDate min = minDate.equals("") ? today.minusDays(365) : LocalDate.parse(minDate);
+		
+		/* Expressão condicional ternária. Se maxDate.equals("") for verdade, ou seja, seja nulo/vazio, max receberá 
+		 * today. Se maxDate.equals("") não seja vazio, ele receberá o LocalDate.parse(maxDate) */
+		// O LocalDate.parse(maxDate) serve para converter o tipo de dado String, do maxDate, para o formato LocalDate
+		LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
+		
+		return repository.findSales(min, max, pageable);
 	}
 
 }
